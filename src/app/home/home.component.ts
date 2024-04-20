cd51931385b9e83f551f77879d30a12aaccd5511
Krystian Derdzinski <krystian.derdzinski@gmail.com>
2024-04-20 14:30:51 +0200

    drop shadow

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,4 +9,18 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  addShadow($event: Event) {
+    const target = $event.target as HTMLElement;
+    target.classList.add('has-shadow');
+  }
+  removeShadow($event: Event) {
+    const target = $event.target as HTMLElement;
+    target.classList.remove('has-shadow');
+  }
+}
+
+// uzywajac (mouseenter) i (mouseout) dodaj drop-shadow przy najechaniu kursorem na item-card
+// 1. przy mouseenter na app-item-card
+// dodajemy do niego klase has-shadow
+// przy mouseout usuwamy klase has-shadow
